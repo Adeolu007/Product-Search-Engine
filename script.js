976acb693f466a1dcaf221a723760b01bcb92b71
Adeolu Odunuyi <93635641+Adeolu007@users.noreply.github.com>
2023-02-13 13:15:39 -0800

    Updated script.js

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
-const fashion = document.querySelector('.fashion')
+
 const search = ()=>{
     const searchbox = document.getElementById('search-item').value.toUpperCase();
     const storeitems = document.getElementById('product-list')
     const product = document.querySelectorAll('.product')
-    const pname = document.getElementsByTagName('h2')
+    const pname = storeitems.getElementsByTagName('h2')
+
+    for (let i=0; i<pname.length;i++){
+        let match = product[i].getElementsByTagName('h2')[0]
+
+        if(match){
+          let textValue = match.textContent || match.innerHTML
+
+          if(textValue.toUpperCase().indexOf(searchbox) > - 1){
+            product[i].style.display = '';
+          } else {
+            product[i].style.display = 'none';
+          }
+
+
+        }
+        
+    }
 }
 
-//  console.log(fashion.value)
-
-//  console.log(4)
-
-fashion.addEventListener('keydown', function(e){
-e.preventDefault();
-if(e.key==='Enter')(
-    console.log(e.target)
-)
-})
